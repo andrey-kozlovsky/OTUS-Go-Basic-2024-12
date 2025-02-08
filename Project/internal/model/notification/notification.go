9f2e7a9fd4b5d93a59ee7007e8d7af2d85a6f06c
Andrey Kozlovsky <andrey.kozlovsky@mego.travel>
2025-02-08 21:14:44 +0300

    OTUS-Go-Basic: Homework 5

---
+++ Project/internal/model/notification/notification.go
@@ -0,0 +1,36 @@
+package notification
+
+import "time"
+
+// Notification представляет сущность уведомления.
+type Notification struct {
+	id      int       // Приватное поле
+	Message string    // Публичное поле
+	SentAt  time.Time // Публичное поле
+	isRead  bool      // Приватное поле
+}
+
+// NewNotification создает новое уведомление.
+func NewNotification(id int, message string) *Notification {
+	return &Notification{
+		id:      id,
+		Message: message,
+		SentAt:  time.Now(),
+		isRead:  false,
+	}
+}
+
+// GetID возвращает ID уведомления.
+func (n *Notification) GetID() int {
+	return n.id
+}
+
+// MarkAsRead отмечает уведомление как прочитанное.
+func (n *Notification) MarkAsRead() {
+	n.isRead = true
+}
+
+// IsRead возвращает статус уведомления (прочитано/не прочитано).
+func (n *Notification) IsRead() bool {
+	return n.isRead
+}
